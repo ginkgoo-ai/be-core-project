fcd2d98110928ec9a8fa39c5c8279693add7e058
seasonsolt <seasonsolt@gmail.com>
2025-03-26 10:05:17 +0800

    fix:add construct for UserInfoResponse

--- src/main/java/com/ginkgooai/core/project/client/identity/dto/UserInfoResponse.java
+++ src/main/java/com/ginkgooai/core/project/client/identity/dto/UserInfoResponse.java
@@ -1,13 +1,21 @@
 package com.ginkgooai.core.project.client.identity.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
 import java.util.*;
 
 @Data
 @Builder
-public class UserInfoResponse {
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserInfoResponse implements Serializable {
+
+    private static final long serialVersionUID = 835098014158567328L;
+
     private String id;
     
     private String sub;
