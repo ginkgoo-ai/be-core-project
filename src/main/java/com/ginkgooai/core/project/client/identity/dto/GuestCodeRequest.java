30fc2f71809b1855992ffa3c0f603889fa85c898
seasonsolt <seasonsolt@gmail.com>
2025-03-26 10:32:18 +0800

    fix:add construct for feign related dto

--- src/main/java/com/ginkgooai/core/project/client/identity/dto/GuestCodeRequest.java
+++ src/main/java/com/ginkgooai/core/project/client/identity/dto/GuestCodeRequest.java
@@ -1,11 +1,20 @@
 package com.ginkgooai.core.project.client.identity.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
 
 @Data
 @Builder
-public class GuestCodeRequest {
+@NoArgsConstructor
+@AllArgsConstructor
+public class GuestCodeRequest implements Serializable {
+   
+    private static final long serialVersionUID = 1L;
+    
     private String workspaceId;
     
     private String resource;
--- src/main/java/com/ginkgooai/core/project/client/storage/dto/CloudFileResponse.java
+++ src/main/java/com/ginkgooai/core/project/client/storage/dto/CloudFileResponse.java
@@ -1,13 +1,21 @@
 package com.ginkgooai.core.project.client.storage.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Schema(description = "Cloud file data transfer object")
-public class CloudFileResponse {
+public class CloudFileResponse implements Serializable {
+    
+    private static final long serialVersionUID = 1L;
 
     @Schema(description = "File ID")
     private String id;
